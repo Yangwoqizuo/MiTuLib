73772f9a5a64eedfa048dad5cf4d1f6dbc7164ea
Yangwoqizuo <123@4567890.xyz>
2019-06-25 14:31:24 +0800

    初始位置修改

--- mitulib/Mitu.py
+++ mitulib/Mitu.py
@@ -8,12 +8,12 @@ def make(element_list):
     result = {
         "ver": "1.0.0",
         "pos": {
-            "x": 0,
-            "y": 0
+            "x": 528,
+            "y": 322
         },
         "scale": {
-            "x": 1.0,
-            "y": 1.0
+            "x": 1.5,
+            "y": 1.5
         },
         "elementGroup": [{
             "pos": {
