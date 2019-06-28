837c8dd7c64fe6d42e3024589afbebbaf968740f
Yangwoqizuo <123@4567890.xyz>
2019-06-28 16:36:27 +0800

    歌唱祖国 铃声加快，改为两种乐器

--- demo.py
+++ demo.py
@@ -1,9 +1,12 @@
 from MiTulib import *
 
 order = MiTu.make([BeginElement(BeginElement.MODE_FREE),
-                   MidiElement(0, 0, "A")])
+                   IfElement("A", "==", "1",
+                             then_do=[VarAssignmentElement("A", 1)],
+                             else_do=[VarAssignmentElement("B", 2)]),
+                   VarAssignmentElement("C", 3)])
 id_in_database = 21
 
 sql = "update `ArchiveTable` set `ArchiveJSON` = '" + order + "' where ArchiveID = " + str(id_in_database) + ";"
 
-print(order)
+print(sql)
--- demo_piano_ringtone.py
+++ demo_piano_ringtone.py
@@ -1,20 +1,27 @@
 from MiTulib import *
 
-half_beat = 0.3
-tones = [9, 11,  14, 9, 11, 14, 9, 10,
+half_beat = 0.2
+tones = [9, 11, 14, 9, 11, 14, 9, 10,
          8, 11, 15, 8, 11, 15, 8, 9,
          7, 10, 14, 7, 10, 14, 7, 10,
          8, 11, 15, 8, 11, 15, 8, 11]
 toneElements = []
-for tone in tones:
-    toneElements.append(MidiElement(0, tone, 0))
+for index in range(len(tones)):
+    if index < 8:
+        toneElements.append(MidiElement(6, tones[index], 0))
+    elif index < 16:
+        toneElements.append(MidiElement(6, tones[index], 0))
+    elif index < 24:
+        toneElements.append(MidiElement(6, tones[index], 0))
+    else:
+        toneElements.append(MidiElement(6, tones[index], 0))
     toneElements.append(DeltaTimeElement(half_beat))
 elements = [BeginElement(BeginElement.MODE_FREE),
             LoopElement(toneElements),
             ]
 
 order = MiTu.make(elements)
-id_in_database = 28
+id_in_database = 32
 
 sql = "update `ArchiveTable` set `ArchiveJSON` = '" + order + "' where ArchiveID = " + str(id_in_database) + ";"
 
